3e3aa9ea858dfb933480ce832be3a195cdc378de
Kunal Chaturvedi <96841396+KCmean@users.noreply.github.com>
2023-12-21 21:18:10 +0530

    Time: 681 ms (27.51%), Space: 10.7 MB (33.19%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        
+        int count = 0;
+        vector<bool> prime(n+1,true);
+        
+        prime[0] = prime[1] = false;
+        
+        for(int i =2 ; i < n; i++){
+            if(prime[i]){
+                count ++;
+                
+                for(int j = 2*i; j<n; j += i){
+                    prime[j] = 0;
+                }
+            }
+        }
+        return count;
+        
+    }
+};
