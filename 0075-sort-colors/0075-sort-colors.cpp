bd930ee11329b3ced1254386e19c260ce870b3ab
Kunal Chaturvedi <96841396+KCmean@users.noreply.github.com>
2024-01-30 11:44:22 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (6.31%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        
+//         vector ans;
+//         map<int,int> umap;
+        
+//         for(auto x : nums){
+//             umap[x]++;
+//         }
+        
+//         for(int i = 0; i < nums.size(); i++ ){
+            
+//         }
+    
+        sort(nums.begin(),nums.end());
+            
+        // return nums;
+        
+        
+    }
+};
