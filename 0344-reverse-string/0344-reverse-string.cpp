55d866f190ca8133ecdf384ed05af1e2c26e7dc3
Kunal Chaturvedi <96841396+KCmean@users.noreply.github.com>
2024-02-04 04:57:22 +0530

    Time: 25 ms (16.19%), Space: 27.9 MB (6.24%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,24 +1,20 @@
 class Solution {
-private:
-    vector<char> stringRev(int i , int j , vector<char>& str){
-    if(i > j){
-        return str;
-    }
-        swap(str[i],str[j]);
-        i++;
-        j--;
-        stringRev( i, j, str);
-
-        return str ;
-}
 public:
     void reverseString(vector<char>& s) {
-
-        int i = 0;
-        int j = s.size() - 1;
-
-
-        s =  stringRev(i,j,s);
+        stack<char> st;
+        
+        for(auto i : s){
+            st.push(i);
+        }
+        
+        string ams;
+        
+        for(int i = 0; i < s.size(); i++){
+            char ch = st.top();
+            s[i] = ch;
+            st.pop();
+            
+        }
         
     }
 };
