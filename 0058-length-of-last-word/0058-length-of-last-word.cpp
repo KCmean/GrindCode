919267dd88f2d705642852f58b7ddad63c4cfb74
Kunal Chaturvedi <96841396+KCmean@users.noreply.github.com>
2024-02-10 20:40:57 +0530

    Time: 4 ms (38.41%), Space: 7.6 MB (21.39%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+       int n = s.length()-1;
+       
+
+       while( n >= 0 && s[n] == ' '){
+           n--;
+       }
+
+       int cnt = n;
+
+       while(cnt >= 0 && s[cnt] != ' '){
+           cnt--;
+       }
+
+        return n - cnt;
+
+
+
+
+        
+    }
+};
