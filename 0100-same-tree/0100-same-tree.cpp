9c598ac8139053f230ed8ad5c26296e1c0d5576d
Kunal Chaturvedi <96841396+KCmean@users.noreply.github.com>
2024-05-05 18:47:51 +0530

    Time: 0 ms (100.00%), Space: 11.8 MB (44.12%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,10 +12,33 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p || !q){
-            return p == q;
+        
+        if(p == NULL && q == NULL){
+            return true;
         }
-        return p->val == q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right) ;
+         if(p == NULL && q != NULL){
+            return false;
+        }
+         if(p != NULL && q == NULL){
+            return false;
+        }
+        
+        
+        bool left = isSameTree(p->left , q -> left);
+        bool right = isSameTree(p->right , q -> right);
+        
+        bool value = p -> val == q -> val;
+        
+        if(left && right && value){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+        
+        
+
         
     }
 };
